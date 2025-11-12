85797b3bb63f1a1e7a44200d8a2fed201bc804b1
White Cai <whitecai@usc.edu>
2025-11-12 05:37:00 +0000

    equal path

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,32 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool equalPathsHelper(Node* node, int& height) {
+    if (!node) {               
+        height = -1;
+        return true;
+    }
 
+    int hl, hr;
+    bool okL = equalPathsHelper(node->left,  hl);
+    bool okR = equalPathsHelper(node->right, hr);
+
+    
+    height = 1 + (hl > hr ? hl : hr);
+
+    if (!okL || !okR) return false;
+
+    
+    if (node->left != nullptr && node->right != nullptr) {
+        return hl == hr;
+    }
+    return true;
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+     int h;
+    return equalPathsHelper(root, h);
 }
 
